4de69ef10ed6e9097f083f27a6311128b1d4ab60
AlexBrentCode <alexbrentcode@gmail.com>
2021-03-31 17:18:23 +0100

    AppDatabase abstract class implemented and added to QuestionDAO

--- app/src/main/java/com/example/studycards/QuestionDAO.java
+++ app/src/main/java/com/example/studycards/QuestionDAO.java
@@ -1,11 +1,37 @@
 package com.example.studycards;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
 
+import java.util.List;
+
+@Dao
 public interface QuestionDAO {
 
     @Insert
     public void insertQuestion(String text, String answer, int categoryID);
 
+    @Query("SELECT QuestionText from Question")
+    List<String> getAllQuestions();
+
+/*
+    @Query("UPDATE QuestionText from Question where QuestionText = :QuesionText")
+    public void updateQuesstion(String QuestionText);
+*/
+
+    @Update
+    public void updateQuestion(Question question);
+
+    @Delete
+    public void deleteAnswer(String AnswerText);
+
+    @Delete
+    public void deleteQuestion(String QuestionText);
+
+    @Delete
+    public void deleteAllQuestions();
 
 }
