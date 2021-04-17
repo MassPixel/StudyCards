e61973107c6be81c71b401b69e49070759f89ba0
MassPixel <64043878+MassPixel@users.noreply.github.com>
2021-04-17 15:42:25 +0100

    fully implemented fragment asking all questions randomly, weighted by comfort

--- app/src/main/java/com/example/myapplication1/data/CategoryQuestionCrossRef.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication1.data;
-
-import androidx.room.Entity;
-
-@Entity(primaryKeys = {"CategoryID", "QuestionID"})
-public class CategoryQuestionCrossRef {
-    public int CategoryID;
-    public int QuestionID;
-}
--- app/src/main/java/com/example/studycards/CategoryQuestionCrossRef.java
+++
@@ -1,9 +0,0 @@
-package com.example.studycards;
-
-import androidx.room.Entity;
-
-@Entity(primaryKeys = {"CategoryID", "QuestionID"})
-public class CategoryQuestionCrossRef {
-    public int CategoryID;
-    public int QuestionID;
-}
